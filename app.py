49c2a43d2d609da303a60c87e7ea8e18f103b4cd
superangea <jiaqi_cheng@brown.edu>
2015-10-03 15:48:30 -0400

    Facebook integrate

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+#!/usr/bin/env python 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Hello World!"
+  return render_template('index.html')
 
 
 if __name__ == '__main__':
