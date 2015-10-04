4c9cb78ba5c394aba2ee07e1c365399b095d2ed9
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-10-03 20:11:02 -0400

    webcam working

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/test')
+def test():
+	return render_template('test.html')
+
+@app.route('/test2')
+def test2():
+	return render_template('test2.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
