fd10c29b4ff793c3bf836468bf7238d5f98fcacb
superangea <jiaqi_cheng@brown.edu>
2015-10-03 21:19:46 -0400

    Html files updated

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python 
 from flask import Flask, render_template, request, url_for, redirect, make_response
-from instagram import client
+from instagram.client import InstagramAPI
 
 app = Flask(__name__)
 
 client_id = 'b8e0e0bf4c214c9f920bc630374b27b3'
 client_secret = 'b5c36307fef8432b9233d6ab436b3b18'
 redirect_uri = 'http://localhost:5000/oauth_callback'
+access_token = '2222183970.b8e0e0b.d7f43b7e651a46fbbf0b1b71e40fb217'
 
+api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+# popular_media = api.media_popular(count=20)
+# for media in popular_media:
+#     print media.images['standard_resolution'].url
+
+# media_feed, next = api.user_media_feed()
+# photos = []
+# for media in media_feed:
+# 	print media.get_standard_resolution_url()
+	# photos.append('<img src="%s"/>' % media.get_standard_resolution_url())
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/test')
-def test():
-	return render_template('test.html')
+# # @app.route('/redirectaccess', methods=['POST', 'GET'])
+# # def on_callback():
+# #   return render_template('index.html')
 
-@app.route('/test2')
-def test2():
-	return render_template('test2.html')
+# @app.route('/test')
+# def test():
+# 	return render_template('test.html')
+
+# @app.route('/test2')
+# def test2():
+# 	return render_template('test2.html')
 
 
 
---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+var sampleDescription = "GD pizza party #risd #gd #rhodeislandschoolofdesign #graphic design";
+var sampleDate = "September 9, 2015 at 11:40am";
+var sampleName = "soeugene";
+
+$(document).ready(function(){
+	$(".like-description").text("Sam Song and 28,341 others like this " + "And 1,000 Smiles");
+	$(".description").text(sampleDescription);
+	$(".date").text(sampleDate);
+	$(".user-name").text(sampleName);
+	$(".see-more").text("see more");
+});
+
